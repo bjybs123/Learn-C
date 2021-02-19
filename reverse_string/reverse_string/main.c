6c09501ff6d9386ae3acfa126b3fb5ef4527d04d
Jiyong Park <bjybs123@outlook.com>
2021-02-20 00:38:16 +0900

    Project 13-16

---
+++ reverse_string/reverse_string/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char *message);
+
+int main()
+{
+    char line[10] = "KEXT";
+    printf("%s\n", line);
+    reverse(line);
+    printf("%s\n", line);
+}
+
+void reverse(char *message)
+{
+    char *p1, *p2;
+    int len = strlen(message);
+    p1 = message;
+    p2 = message + len - 1;
+    
+    while(p1 <= p2)
+    {
+        int temp;
+        temp = *p2;
+        *p2 = *p1;
+        *p1 = temp;
+        
+        p1++;
+        p2--;
+    }
+}
